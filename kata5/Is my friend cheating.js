77e014b72cec10932fd7b957ec4bd7032cd645fa
Hong Hai <59792233+honghainguyen777@users.noreply.github.com>
2021-03-27 19:59:10 +0100

    A Chain adding function

--- kata5/Is my friend cheating.js
+++ kata5/Is my friend cheating.js
@@ -1,40 +1,40 @@
-// Is my friend cheating?
+// A Chain adding function
 
-// A friend of mine takes the sequence of all numbers from 1 to n (where n > 0).
-// Within that sequence, he chooses two numbers, a and b.
-// He says that the product of a and b should be equal to the sum of all numbers in the sequence, excluding a and b.
-// Given a number n, could you tell me the numbers he excluded from the sequence?
-// The function takes the parameter: n (n is always strictly greater than 0) and returns an array or a string (depending on the language) of the form:
+// We want to create a function that will add numbers together when called in succession.
+// function add(n){
+//   let currentSum = n;
+//   function addF(x) {
+//     currentSum += x;
+//     return addF;
+//   }
+//   addF.valueOf = function() {
+//     return currentSum;
+//   }
+//   return addF;
+// }
 
-// [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or or [{a, b}, ...]
-// with all (a, b) which are the possible removed numbers in the sequence 1 to n.
-
-// [(a, b), ...] or [[a, b], ...] or {{a, b}, ...} or ... will be sorted in increasing order of the "a".
-
-// It happens that there are several possible (a, b). The function returns an empty array (or an empty string) if no possible numbers are found which will prove that my friend has not told the truth! (Go: in this case return nil).
-function removeNb (n) {
-  const preSum = (n * (n +1))/2;
-  const result = [];
-  for (let a = 1; a <= n; a++) {
-    const b = (preSum - a)/(a + 1);
-    if (Number.isInteger(b) && b <= n ) result.push([a, b]);
-  }
-  return result;
+function add(n) {
+  const f = x => add(n + x);
+  f.valueOf = () => n;
+  return f;
 }
-removeNb(26);
+add(1)(2);
+// // returns 3
+// We also want to be able to continue to add numbers to our chain.
 
-// Examples:
-// removNb(26) should return [(15, 21), (21, 15)]
-// or
-// removNb(26) should return { {15, 21}, {21, 15} }
-// or
-// removeNb(26) should return [[15, 21], [21, 15]]
-// or
-// removNb(26) should return [ {15, 21}, {21, 15} ]
-// or
-// removNb(26) should return "15 21, 21 15"
-// or
+// add(1)(2)(3); // 6
+// add(1)(2)(3)(4); // 10
+// add(1)(2)(3)(4)(5); // 15
+// and so on.
 
-// in C:
-// removNb(26) should return  {{15, 21}{21, 15}} tested by way of strings.
-// Function removNb should return a pointer to an allocated array of Pair pointers, each one also allocated. 
+// A single call should return the number passed in.
+
+// add(1); // 1
+// We should be able to store the returned values and reuse them.
+
+// var addTwo = add(2);
+// addTwo; // 2
+// addTwo + 5; // 7
+// addTwo(3); // 5
+// addTwo(3)(5); // 10
+// We can assume any number being passed in will be valid whole number.
